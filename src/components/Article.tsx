1a715c4cafb5fc7e811484d7b6f08fc303247d2e
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-03 23:28:11 -0400

    Adjust Style component Article

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -4,7 +4,7 @@ export function Article() {
   return (
     <Link
       href="/"
-      className="flex flex-col px-4 py-4 border border-b-slate-50 md:px-6 md:py-6"
+      className="flex flex-col px-4 py-4 border-b-[1px] md:px-6 md:py-6 transition-background hover:bg-slate-100"
     >
       <time className="text-sm text-zinc-500">12 de março de 2021</time>
       <strong className="mt-3 mb-1 text-lg text-zinc-800 lg:text-xl">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
+import Head from "next/head";
 import { Article } from "components/Article";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-    </div>
+    <main className="min-h-screen max-w-[1000px] my-8 mx-auto">
+      <Head>
+        <title>Innovea | Home</title>
+      </Head>
+      <section className="flex flex-col gap-4">
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+      </section>
+    </main>
   );
 }
